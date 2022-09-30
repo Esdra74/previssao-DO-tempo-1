8bb65ab759b1f22ac97b5998f84193eea6ed3fc2
Esdra74 <110417696+Esdra74@users.noreply.github.com>
2022-09-29 23:38:39 -0300

    refatorando-weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,18 @@
 
 
+
 const APIKey = 'pyKG2EnzcpE30RwldHY6fLV4XY5aGaGC'
-const getcityUrl = cityName => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+const baseUrl = 'http://dataservice.accuweather.com/'
+
+
+const getcityUrl = cityName => `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getWeatherUrl = ({ Key }) => 
+    `${baseUrl}currentconditions/v1/${Key}?apikey=${APIKey}`
 
 const fetchData = async url => {
     try {
+    
         const response = await fetch(url)
 
         if (!response.ok) {
@@ -19,27 +27,10 @@ const fetchData = async url => {
 
 const getCityData = cityName => fetchData(getcityUrl(cityName))
 
-getCityData('São Paulo')
-.then(console.log)
-
-/*const getCityWeather = async cityName => {
-    try {
-        const { Key } = await getCityData(cityName)
-        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
-        const response = await fetch(cityWeatherUrl)
-
-        if (!response.ok) {
-            throw new Error('não foi possivel obter os dados')
-        }
-
-       const [cityWeatherData] = await response.json()
-       
-       return cityWeatherData
-    }catch ({name, message }) {
-        alert(`${name}: ${message}`)
-    }
+    const getCityWeather = async cityName => {
+    const [cityData] = await getCityData(cityName)
+    return fetchData(getWeatherUrl(cityData))
 }
 
 getCityWeather('São Paulo')
-
-*/
+.then(console.log)
